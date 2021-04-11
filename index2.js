12e9ac88e878f189a68b2095a6ac51239169c55e
baraaAlosaily <seelbaraa@gmail.com>
2021-04-11 19:53:40 +0300

    new

--- index2.js
+++ index2.js
@@ -1,18 +1,18 @@
-// let yourName = prompt('Please enter your name');
-// alert(`Welcome ` + yourName + `to learn for future`)
-// console.log(`Welcome ` + yourName + `to learn for future`);
+let yourName = prompt('Please enter your name');
+alert(`Welcome ` + yourName + `to learn for future`)
+console.log(`Welcome ` + yourName + `to learn for future`);
 
-// let yourAge = prompt(`How old ara you`)
-// alert(`you old is ` + yourAge)
-// console.log(`you old is ` + yourAge);
+let yourAge = prompt(`How old ara you`)
+alert(`you old is ` + yourAge)
+console.log(`you old is ` + yourAge);
 
-// let faVa = prompt('Let us know your favorate courses')
-// alert(`We have wide materials about ` + faVa)
-// console.log(`We have wide materials about ` + faVa);
+let faVa = prompt('Let us know your favorate courses')
+alert(`We have wide materials about ` + faVa)
+console.log(`We have wide materials about ` + faVa);
 
-// let coUrse = prompt(`Did you have any last experience with online cources`)
-// alert(`Geart!!!`)
-// console.log(`Geart!!!`);
+let coUrse = prompt(`Did you have any last experience with online cources`)
+alert(`Geart!!!`)
+console.log(`Geart!!!`);
 
-// alert(`welcome ` + yourName + ` to our world, you are ` + yourAge + ` we will get alot of knowledge here ` + ` we have special materials about ` + faVa + ` complete search `)
-// console.log(`welcome ` + yourName + ` to our world, you are ` + yourAge + ` we will get alot of knowledge here ` + ` we have special materials about ` + faVa + ` complete search `);
+alert(`welcome ` + yourName + ` to our world, you are ` + yourAge + ` we will get alot of knowledge here ` + ` we have special materials about ` + faVa + ` complete search `)
+console.log(`welcome ` + yourName + ` to our world, you are ` + yourAge + ` we will get alot of knowledge here ` + ` we have special materials about ` + faVa + ` complete search `);
